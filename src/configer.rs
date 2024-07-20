c690a306164b37c63973957487d121c13cf59c73
Dmytro Shtrikker <shtrikker28@gmail.com>
2024-07-20 19:29:13 +0300

    add config parse result handling
    Signed-off-by: Dmytro Shtrikker <shtrikker28@gmail.com>

--- src/configer.rs
+++ src/configer.rs
@@ -14,72 +14,73 @@ pub struct Config {
 
 impl Config {
     pub fn new() -> Self {
-        let mut config = Config {
+        Config {
             api_key: String::default(),
             region_id: String::default(),
-        };
-        let content = config.read_config();
-        config.parse_content(&content);
-        config
+        }
     }
 
     pub fn get_api_key(&self) -> &String {
+        if self.api_key.is_empty() {
+            eprintln!("api_key is empty, did you run parse?");
+        }
         &self.api_key
     }
 
     pub fn get_regions_id(&self) -> &String {
+        if self.region_id.is_empty() {
+            eprintln!("region_id is empty, did you run parse?");
+        }
         &self.region_id
     }
 
-    fn read_config(&mut self) -> String {
+    pub fn parse(&mut self) -> bool {
         let mut file = match File::open(CONFIG_PATHNAME) {
             Ok(file) => file,
             Err(err) => {
                 eprintln!("Failed to open file: {}", err);
-                return "".to_string();
+                return false;
             }
         };
 
-        let mut contents = String::new();
-        match file.read_to_string(&mut contents) {
+        let mut content = String::new();
+        match file.read_to_string(&mut content) {
             Ok(_) => (),
             Err(err) => {
                 eprintln!("Failed to read file content: {}", err);
-                return "".to_string();
+                return false;
             }
         }
 
-        contents
-    }
-
-    fn parse_content(&mut self, content: &str) {
-        let parsed = match json::parse(content) {
+        let parsed = match json::parse(content.as_str()) {
             Ok(parsed) => parsed,
             Err(err) => {
                 eprintln!("Failed to parse content: {}", err);
-                return;
+                return false;
             }
         };
 
         let api_key_obj = &parsed[API_KEY_FIELD];
         if api_key_obj.is_null() {
             eprintln!("Failed to parse content: {} field is null", API_KEY_FIELD);
-            return;
+            return false;
         }
         if api_key_obj == PLACEHOLDER_API_KEY {
             eprintln!(
                 "Failed to parse content: fill {} with your key",
                 API_KEY_FIELD
             );
-            return;
+            return false;
         }
         self.api_key = api_key_obj.to_string();
 
         let region_id_obj = &parsed[REGION_ID_FIELD];
         if region_id_obj.is_null() {
             eprintln!("Failed to parse content: {} field is null", REGION_ID_FIELD);
-            return;
+            return false;
         }
         self.region_id = region_id_obj.to_string();
+
+        true
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ use configer::Config;
 use observer::Observer;
 
 fn main() {
-    let config = Config::new();
-    let mut observer = Observer::new(config.get_api_key(), config.get_regions_id());
-    observer.poll();
+    let mut config = Config::new();
+    if config.parse() {
+        let mut observer = Observer::new(config.get_api_key(), config.get_regions_id());
+        observer.poll();
+    }
 }
